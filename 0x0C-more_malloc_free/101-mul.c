c78642f2012ddd3e3000af9c1ef50b45e3d6b999
KimberlyPeters <kimberlypeters468@gmail.com>
2023-03-16 12:06:58 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,51 +1,59 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int str_len(char *str)
-{
-    int len = 0;
-    while (*str)
-    {
-        if (*str >= '0' && *str <= '9')
-            len++;
-        else
-            exit(98);
-        str++;
+// Function to multiply two numbers in string format
+char* multiply(char* num1, char* num2) {
+    int len1 = strlen(num1);
+    int len2 = strlen(num2);
+    int len = len1 + len2;
+    int i, j, k, carry, sum;
+    char* result = (char*)malloc((len + 1) * sizeof(char));
+    memset(result, '0', len);
+    result[len] = '\0';
+    for (i = len1 - 1; i >= 0; i--) {
+        carry = 0;
+        for (j = len2 - 1; j >= 0; j--) {
+            sum = (num1[i] - '0') * (num2[j] - '0') + (result[i + j + 1] - '0') + carry;
+            carry = sum / 10;
+            result[i + j + 1] = (sum % 10) + '0';
+        }
+        result[i + j + 1] += carry;
     }
-    return len;
-}
-
-int power(int base, int exp)
-{
-    int result = 1;
-    while (exp--)
-        result *= base;
-    return result;
-}
-
-int str_to_int(char *str)
-{
-    int result = 0;
-    int len = str_len(str);
-    while (*str)
-    {
-        result += (*str - '0') * power(10, len - 1);
-        len--;
-        str++;
+    for (k = 0; k < len && result[k] == '0'; k++);
+    if (k == len) {
+        result[0] = '0';
+        result[1] = '\0';
+        return result;
     }
-    return result;
+    return &result[k];
 }
 
-int main(int argc, char **argv)
-{
-    if (argc != 3)
-    {
+// Function to check if a string contains only digits
+int is_digit(char* str) {
+    int i;
+    for (i = 0; str[i] != '\0'; i++) {
+        if (!isdigit(str[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
         printf("Error\n");
         return 98;
     }
-    int num1 = str_to_int(argv[1]);
-    int num2 = str_to_int(argv[2]);
-    int result = num1 * num2;
-    printf("%d\n", result);
+    char* num1 = argv[1];
+    char* num2 = argv[2];
+    if (!is_digit(num1) || !is_digit(num2)) {
+        printf("Error\n");
+        return 98;
+    }
+    char* result = multiply(num1, num2);
+    printf("%s\n", result);
+    free(result);
     return 0;
 }
-
