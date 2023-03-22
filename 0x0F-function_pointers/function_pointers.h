dbe8032f53bf1ac6d10aa3a55429605343a5b41d
KimberlyPeters <kimberlypeters468@gmail.com>
2023-03-22 09:25:22 -0700

    Update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,6 +7,8 @@
  */
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
