3ad516102757b74aa1263f739498b7da51ada637
KimberlyPeters <kimberlypeters468@gmail.com>
2023-04-04 07:11:15 -0700

    Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,50 +1,85 @@
 #include "lists.h"
-
 /**
- * free_listint_safe - frees a linked list, even if it contains a loop
- * @h: pointer to the head of the list
+ * free_listp2 - frees a linked list
+ * @head: head of a list.
  *
- * Return: size of the list that was freed
+ * Return: no return.
+ */
+void free_listp2(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+/**
+ * free_listint_safe - Frees a listint_t list safely (ie.
+ *                     can free lists containing loops)
+ * @h: A pointer to the address of
+ *     the head of the listint_t list.
+ *
+ * Return: The size of the list that was freed.
+ *
+ * Description: The function sets the head to NULL.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count = 0;
-	listint_t *tmp, *loop_start;
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
+	listint_t *curr;
 
-	if (h == NULL || *h == NULL)
-		return (count);
+	hptr = NULL;
+	while (*h != NULL)
+	{
+		new = malloc(sizeof(listp_t));
 
-	loop_start = find_listint_loop(*h);
-	if (loop_start == NULL)
-	{
-		while (*h != NULL)
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
 		{
-			tmp = (*h)->next;
-			free(*h);
-			*h = tmp;
-			count++;
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_listp2(&hptr);
+				return (nnodes);
+			}
 		}
+
+		curr = *h;
+		*h = (*h)->next;
+		free(curr);
+		nnodes++;
 	}
-	else
-	{
-		while (*h != loop_start)
-		{
-			tmp = (*h)->next;
-			free(*h);
-			*h = tmp;
-			count++;
-		}
-		while ((*h)->next != loop_start)
-		{
-			tmp = loop_start->next;
-			free(loop_start);
-			loop_start = tmp;
-			count++;
-		}
-		free(loop_start);
-		count++;
-		*h = NULL;
-	}
-	return (count);
+
+	*h = NULL;
+	free_listp2(&hptr);
+	return (nnodes);
 }
 
+/**
+ * free_listp_safe - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listp_safe(listp_t **head)
+{
+	free_listp2(head);
+}
