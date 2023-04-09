ec122ed110e265dfd6695814651da760cec31efe
KimberlyPeters <kimberlypeters468@gmail.com>
2023-04-09 05:29:20 -0700

    Header file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,28 +8,26 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int i;
+	int len = 0, i;
+	unsigned int sum = 0, base = 1;
 
-	if (*b == '\0')
+	if (b == NULL)
 		return (0);
-	
-	while (b[i] != '\0')
+
+	while (b[len])
 	{
-		if (b[i] == '0')
-		{
-			num <<= 1;
-		}
-		else if (b[i] == '1')
-		{
-			num <<= 1;
-			num += 1;
-		}
-		else
-		{
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
-		}
-		i++;
+		len++;
 	}
-	return (num);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+		{
+			sum += (1 * base);
+		}
+		base *= 2;
+	}
+	return (sum);
 }
