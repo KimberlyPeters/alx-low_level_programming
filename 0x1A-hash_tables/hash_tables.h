0f4b4dc4da06af71dc85dbbea55364a06ff43548
KimberlyPeters <kimberlypeters468@gmail.com>
2023-06-09 07:04:18 -0700

    Header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -83,6 +83,13 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 /* 3-hash_table_set.c */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
+/* 4-hash_table_get.c */
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
+/* 5-hash_table_print.c */
+void hash_table_print(const hash_table_t *ht);
+
+/* 6-hash_table_delete.c */
+void hash_table_delete(hash_table_t *ht);
 
 #endif
