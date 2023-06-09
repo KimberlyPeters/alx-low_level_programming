aceba219e48317b81b1ba67105a0c5f4c480bfd7
KimberlyPeters <kimberlypeters468@gmail.com>
2023-06-09 06:32:36 -0700

    index of a key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -14,7 +14,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 
 	if (key == NULL || size == 0)
 		return (0);
-	
+
 	index = hash_djb2(key);
 
 	return (index % size);
